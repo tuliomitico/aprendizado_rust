b2c7c5818e3e3c4c68f9fef06ad0c583a19d3709
tuliomitico <tulioneedforspeed@gmail.com>
2024-03-06 15:35:03 -0300

    Entrada de dados e import de modulos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-fn main() {
-    let number1 = 5;
-    let number2 = 5;
-    if number1 > number2 {
-        println!("{} > {}", number1, number2);
+use std::io;
+
+fn convert_to_int(data_input: & String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+fn main() -> (){
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("Erro ao ler number1");
+    let mut number2 = String::new();
+    io::stdin().read_line(&mut number2).expect("Erro ao ler number2");
+    
+    if convert_to_int(&number1) > convert_to_int(&number2){
+        println!("O numero {} eh maior que {}", number1, number2);
     } else {
-        println!("{} <= {}", number1, number2);
+        println!("O numero {} eh menor ou igual que {}", number1, number2);
     }
 }
