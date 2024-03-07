4b40b3a9d20719e1ad517c5ce2e7b47ef59c60ef
tuliomitico <tulioneedforspeed@gmail.com>
2024-03-07 15:01:24 -0300

    Soma de digitos

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,15 @@ fn convert_to_int(data_input: & String) -> i32 {
     x
 }
 fn main() -> (){
-    let mut number1 = String::new();
-    io::stdin().read_line(&mut number1).expect("Erro ao ler number1");
-    let mut number2 = String::new();
-    io::stdin().read_line(&mut number2).expect("Erro ao ler number2");
-    
-    if convert_to_int(&number1) > convert_to_int(&number2){
-        println!("O numero {} eh maior que {}", number1, number2);
-    } else {
-        println!("O numero {} eh menor ou igual que {}", number1, number2);
+    let mut soma = 0;
+    let mut valor_entrada = String::new();
+    io::stdin().read_line(&mut valor_entrada).expect("Erro ao ler valor_entrada");
+    let mut valor_i32 = convert_to_int(&valor_entrada);
+
+    while valor_i32 != 0 {
+        let mut r = valor_i32 % 10;
+        soma = soma + r;
+        valor_i32 = valor_i32 / 10;
     }
+    println!("O valor da soma dos digitos {}",soma)
 }
