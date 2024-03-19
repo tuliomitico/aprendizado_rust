ac0b46ba824cdabf4da6cc14c9f3e45e70a86fbd
tuliomitico <tulioneedforspeed@gmail.com>
2024-03-19 15:02:31 -0300

    funcao parte 1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::io; /* importação de biblioteca de input/output padrão */
+fn dobro(num: i32) -> i32 {
+    return 2 * num;
+}
 
-/* fn convert_to_int(data_input: & String) -> i32 { // função para converter valores string em inteiros
-     let x = data_input.trim().parse::<i32>().unwrap();
-     x
-} */
-fn main() -> () {
-    let mut a = 15;
-    let mut b = 40;
-    while b != 0  // enquanto b for diferente de zero continuar no loop while
-    {
-    let temp = b; // armazene  o menor valor das duas variáveis na variável temp
-    b = a % b; // armazene o resto da divisão do valor maior pelo menor
-    a = temp; // armazene o valor menor em temp na variável a
+fn maior(a: i32, b: i32) -> i32 {
+    if a >= b{
+        return a;
     }
-    println!("O maior divisor comum entre 15 e 40 eh: {}", a);
+    else{
+        return b;
+    }
+}
+
+fn main() -> () {
+    println!("O maoir numero entre 5 e 4 eh {}",maior(5,4));
+    println!("O dobro de {} eh: {}",5,dobro(5));
 }
