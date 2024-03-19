ac41998aeb6d95d778a4132d03924e8a4f6aa07c
tuliomitico <tulioneedforspeed@gmail.com>
2024-03-19 14:52:14 -0300

    solucao  do divisor comum

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-use std::io;
+use std::io; /* importação de biblioteca de input/output padrão */
 
-fn convert_to_int(data_input: & String) -> i32 {
-    let x = data_input.trim().parse::<i32>().unwrap();
-    x
-}
+/* fn convert_to_int(data_input: & String) -> i32 { // função para converter valores string em inteiros
+     let x = data_input.trim().parse::<i32>().unwrap();
+     x
+} */
 fn main() -> () {
-    let mut a = String::new();
-    io::stdin().read_line(&mut a).expect("Erro ao ler a");
-    let mut b = String::new();
-    io::stdin().read_line(&mut b).expect("Erro ao ler b");
-    let mut divisao = 0;
-    let mut i = 1; 
-    while i < convert_to_int(&a) && i < convert_to_int( &b) {
-        if convert_to_int( &a) % i == 0 && convert_to_int( &b) % i == 0 {
-            divisao = i;
-        }
-        i += 1;
+    let mut a = 15;
+    let mut b = 40;
+    while b != 0  // enquanto b for diferente de zero continuar no loop while
+    {
+    let temp = b; // armazene  o menor valor das duas variáveis na variável temp
+    b = a % b; // armazene o resto da divisão do valor maior pelo menor
+    a = temp; // armazene o valor menor em temp na variável a
     }
-    println!("O maior divisor comum eh {}",divisao);
+    println!("O maior divisor comum entre 15 e 40 eh: {}", a);
 }
