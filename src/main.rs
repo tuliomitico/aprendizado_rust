a42367be87d9d64ef51154083aaa275a04dab0c1
tuliomitico <tulioneedforspeed@gmail.com>
2024-05-17 20:18:06 -0300

    funcao parte 2

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ fn maior(a: i32, b: i32) -> i32 {
         return b;
     }
 }
+fn alguma_fn(par_a: f32, par_b: i128) -> f32 {
+    println!("Essa função devolve um valor flutuante");
+    10.1
+}
 
 fn main() -> () {
     println!("O maoir numero entre 5 e 4 eh {}",maior(5,4));
