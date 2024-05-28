28d907c783e9a3bd8c746a94351c20b15ef8c464
tuliomitico <tulioneedforspeed@gmail.com>
2024-05-27 21:11:01 -0300

    calculo fatorial

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-fn main() {
-    let animais = ["Coelho","Gato","Macaco"];
+use std::io;
 
-    for a in animais {
-        println!("O animal a seguir eh o {}",a);
-    }   
+fn convert_to_int(data_input: &String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+
+fn main() {
+   let mut entrada_fatorial = String::new();
+   io::stdin().read_line(&mut entrada_fatorial).expect("Erro ao ler entrada_fatorial");
+   let mut fatorial = 1;
+   let mut entrada_int = convert_to_int(&entrada_fatorial);
+
+   while entrada_int > 1 {
+     fatorial = fatorial * entrada_int;
+     entrada_int = entrada_int - 1;
+   }
+   println!("O fatorial eh {}", fatorial);
 }
